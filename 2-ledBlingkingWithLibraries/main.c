80ec265f4db106f53de2542eadbd92088a27e606
Hakan <haknkayaa@gmail.com>
2016-12-18 23:21:53 +0300

    update

--- 2-ledBlingkingWithLibraries/main.c
+++ 2-ledBlingkingWithLibraries/main.c
@@ -1,23 +1,62 @@
-#include "stm32f4xx.h"
-#include "stm32f4_discovery.h"
+/*******************************************************************************
+* STM32F4 - LED Yakip Sondurma
+* ------------------------------------------------------------------------------
+* @author  : Hakan Kaya
+* @mail    : mail@hakankaya.kim
+* @date    : 18.12.2016
+* @brief   : Kart uzerinde bulunan ledleri yakip söndürmek
+* 
+*******************************************************************************/
 
-int main()
-{
-  //Enable the GPIOD Clock
-  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD,ENABLE);
-  
-  // GPIOD Configuration
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_15;
-  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
-  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
-  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
-  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
-  
-  GPIO_Init(GPIOD, &GPIO_InitStruct);
-  
-  // GPIOD-PIN-15 ON
-  GPIO_SetBits(GPIOD, GPIO_Pin_15);
-  
-  return 0;
+// Kütüphaneler
+#include "stm32f4xx.h"
+//#include "stm32f4xx_rcc.h"    // system_conf.h icerisinden cagrildi
+//#include "stm32f4xx_gpio.h"   // system_conf.h icerisinden cagrildi
+
+// Fonksiyon prototipleri
+void canimDelayim(unsigned long zaman);
+void GPIO_Hazirliklari(void);
+
+// Main fonksiyonu
+int main(void) {
+
+   // GPIO islemlerinin oldugu fonksiyonun cagrilmasi
+   GPIO_Hazirliklari();
+   
+   // Sonsuz dongu
+    while (1) {     
+        GPIO_SetBits(GPIOD, GPIO_Pin_All);
+        canimDelayim(2000000);
+        GPIO_ResetBits(GPIOD, GPIO_Pin_All);
+        canimDelayim(2000000);
+    }
+    
+    //return 0;
+}
+
+void canimDelayim(unsigned long zaman){
+  while(zaman > 0){
+    zaman--;
+  }
+}
+
+
+void GPIO_Hazirliklari(void){   
+    // D portu icin RCC aktiflestirme
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    
+    // D portu icin pointer vektoru tanýmlanmasý
+    GPIO_InitTypeDef GPIO_D;
+    
+    // Port ayarlamalari
+    GPIO_D.GPIO_Pin = GPIO_Pin_All; 
+    GPIO_D.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_D.GPIO_OType = GPIO_OType_PP;
+    GPIO_D.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_D.GPIO_Speed = GPIO_Speed_100MHz;
+    
+    // Vektorun tanimlanmasi
+    GPIO_Init(GPIOD, &GPIO_D);
+    
+   
 }
